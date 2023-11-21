1a0e2b08ad645613b3d74b91f33a23c9d476198e
licaret <licaretraul@gmail.com>
2023-11-21 19:29:20 +0200

    Add POST posts endpoint

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const { randomBytes } = require('crypto');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const posts = {
-	"test-id": {
-		id: "test-id",
-		title: "Post Title"
-	}
-};
+const posts = {};
 
 app.get("/posts", (req, res) => {
 	res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+	const { title } = req.body;
+	const id = randomBytes(4).toString("hex");
+
+	posts[id] = { id, title };
+
+	res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
